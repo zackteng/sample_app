13c8fe64647be74ae4a92be382988d6e337a48df
zackteng <tengzack@gmail.com>
2014-01-20 21:45:54 +0800

    six test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,7 +37,7 @@ describe User do
 
 	describe 'when email format is invalid' do
 		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
         	addresses.each do |invalid_address|
         		@user.email = invalid_address
         		expect(@user).not_to be_valid
@@ -97,5 +97,15 @@ describe User do
 	describe "with a password that's too short"	 do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
 		it { should be_invalid }
+	end
+
+	describe 'email address with mixed case' do
+		let(:mixe_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be save as all lower_case" do
+			@user.email = mixe_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixe_case_email.downcase
+		end
 	end
 end
