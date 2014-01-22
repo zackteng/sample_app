d756e374c448d841347a3142ac7f2c8ca6cce970
zackteng <tengzack@gmail.com>
2014-01-22 20:47:28 +0800

    seven test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,13 @@ describe "UserPages" do
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+
+      describe 'after submission' do
+        before { click_button submit }
+
+        it { should have_title("Sign up") }
+        it { should have_content("error") }
+      end
   	end
 
   	describe 'with valid information' do
@@ -27,6 +34,14 @@ describe "UserPages" do
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
   		end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "tengzack@gmail.com") }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
+      end
   	end
 
   	it { should have_content('Sign up') }
